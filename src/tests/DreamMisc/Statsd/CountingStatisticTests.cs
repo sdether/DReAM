f2e03b6b400ef787488622f9d8952f86978fcffb
Arne Claassen <arne@claassen.net>
2011-04-15 17:29:45 -0700

    testing StatsD

---
+++ src/tests/DreamMisc/Statsd/CountingStatisticTests.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * MindTouch Dream - a distributed REST framework 
+ * Copyright (C) 2006-2011 MindTouch, Inc.
+ * www.mindtouch.com  oss@mindtouch.com
+ *
+ * For community documentation and downloads visit wiki.developer.mindtouch.com;
+ * please review the licensing section.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using MindTouch.Statsd;
+using NUnit.Framework;
+
+namespace MindTouch.Dream.Test.Statsd {
+
+    [TestFixture]
+    public class CountingStatisticTests {
+
+        [Test]
+        public void Can_Convert_to_bytes_without_sampling() {
+            var stat = new CountingStatistic("test.foo", 5);
+            var bytes = stat.ToBytes(1);
+            Assert.AreEqual("test.foo:5|c", bytes.FromBytes());
+        }
+
+        [Test]
+        public void Can_Convert_to_bytes_with_10th_sampling() {
+            var stat = new CountingStatistic("test.foo", 5);
+            var bytes = stat.ToBytes(0.1);
+            Assert.AreEqual("test.foo:5|c|@0.1", bytes.FromBytes());
+        }
+
+        [Test]
+        public void Can_Convert_to_bytes_with_100th_sampling() {
+            var stat = new CountingStatistic("test.foo", 5);
+            var bytes = stat.ToBytes(0.01);
+            Assert.AreEqual("test.foo:5|c|@0.01", bytes.FromBytes());
+        }
+    }
+}
---
+++ src/tests/DreamMisc/Statsd/LiveStatsLoggerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using MindTouch.Statsd;
+using NUnit.Framework;
+using MindTouch.Extensions.Time;
+
+namespace MindTouch.Dream.Test.Statsd {
+
+    [TestFixture,Ignore("these tests talk to a live server")]
+    public class LiveStatsLoggerTests {
+
+        [Test]
+        public void Can_log_single_timing_event() {
+            var logger = new StatsLogger(new StatsConfiguration {
+                Host = "50.17.109.171",
+                Port = 8125
+            });
+            logger.Timing("test.timed", 105.Milliseconds());
+        }
+
+        [Test]
+        public void Can_increment_counter() {
+            var logger = new StatsLogger(new StatsConfiguration {
+                Host = "50.17.109.171",
+                Port = 8125
+            });
+            var counter = 0;
+            while(true) {
+                counter++;
+                logger.Increment("test.counter");
+                Thread.Sleep(500);
+                Console.WriteLine("counter: {0}", counter);
+            }
+        }
+    }
+}
