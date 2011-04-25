809b3142143b169dfef6f0db6f7581b405dda20f
Arne Claassen <arne@claassen.net>
2011-04-25 13:40:08 -0700

    reverting some mistaken assemblyinfo changes

--- src/mindtouch.dream.test/Properties/AssemblyInfo.cs
+++ src/mindtouch.dream.test/Properties/AssemblyInfo.cs
@@ -53,10 +53,10 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.2.67")]
-[assembly: AssemblyFileVersion("2.2.2.67")]
-[assembly: GitBranch("extensions")]
-[assembly: GitRevision("b408627d")]
+[assembly: AssemblyVersion("2.2.2.0")]
+[assembly: AssemblyFileVersion("2.2.2.0")]
+[assembly: GitBranch("2.2")]
+[assembly: GitRevision("HEAD")]
 [assembly: GitUri("https://github.com/MindTouch/DReAM")]
 
 #if SIGNED
